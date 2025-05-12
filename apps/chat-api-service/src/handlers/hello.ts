056b8d80f543495a30e19b9ad84b4551863ace98
Tim <tjj9020@users.noreply.github.com>
2025-05-12 11:22:25 -0400

    fix(build): Prefix unused event param in hello handler

--- apps/chat-api-service/src/handlers/hello.ts
+++ apps/chat-api-service/src/handlers/hello.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 /**
  * Sample Lambda function which returns a hello world response
  */
-export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+export const handler = async (_event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
     // Basic response structure
     const response: APIGatewayProxyResultV2 = {
         statusCode: 200,
